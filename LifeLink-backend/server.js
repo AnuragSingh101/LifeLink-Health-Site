1e521d3eefb690005a446f8aca1bd25e00a901a2
Anurag Singh Rajput <anus82101@gmail.com>
2024-09-14 10:04:33 +0530

    connected mongoDB compass with the express server

--- LifeLink-backend/server.js
+++ LifeLink-backend/server.js
@@ -1,6 +1,19 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+require('dotenv').config();
 const app = express()
+
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(mongoURI,)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+
 
 const port = process.env.PORT || 5000;
 
