39871adaa558af7fb98930d592e73815f04fb0cf
Anurag Singh Rajput <anus82101@gmail.com>
2024-09-23 15:24:14 +0530

    did nothing just deleted inventory and donor routes to re-create them

--- LifeLink-backend/models/bloodInventory.js
+++ LifeLink-backend/models/bloodInventory.js
@@ -4,7 +4,7 @@ const bloodInventorySchema = new mongoose.Schema({
   bloodType: { type: String, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], required: true },
   quantity: { type: Number, required: true },
   expirationDate: { type: Date, required: true },
-  donorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Donor', required: true },
+  // donorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Donor', required: true },
   receivedDate: { type: Date, default: Date.now },
   status: { type: String, enum: ['Available', 'Reserved', 'Used', 'Expired'], default: 'Available' }
 });
